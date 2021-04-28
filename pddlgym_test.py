1c99d0a86dc8170c58675a51c3cce5a0d64226f8
gabwaen <gabrielwaengertner@gmail.com>
2021-04-28 15:53:13 -0300

    Adding submodule + small sources

---
+++ pddlgym_test.py
@@ -0,0 +1,11 @@
+import pddlgym
+from pddlgym_planners.ff import FF
+
+# See `pddl/sokoban.pddl` and `pddl/sokoban/problem3.pddl`.
+env = pddlgym.make("PDDLEnvSokoban-v0")
+env.fix_problem_index(2)
+obs, debug_info = env.reset()
+planner = FF()
+plan = planner(env.domain, obs)
+for act in plan:
+    obs, reward, done, info = env.step(act)
