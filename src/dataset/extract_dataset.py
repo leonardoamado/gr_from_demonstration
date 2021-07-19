192cad95e7b2c5e00da8851d9fd0bb43e3c1f7bc
gabwaen <gabrielwaengertner@gmail.com>
2021-07-18 22:13:09 -0300

    Removed more unused code

--- src/dataset/extract_dataset.py
+++ src/dataset/extract_dataset.py
@@ -14,24 +14,15 @@ except KeyError as e:
 
 DATASET_ROOT = f'{REPO_ROOT}/goal-plan-recognition-dataset'
 
-# name_map
 
 # will only consider blocksworld for now
-
-def add_to_pddlgym(envname, ignore_noisy=True):
-
-    pddlgym_path = os.path.join(pddlgym.__file__, 'pddl', envname)
-
-
 def extract(envname, ignore_noisy=True, obs=['100']):
     # assumes that the dataset is located at root/goal-plan-recognition-dataset
     # and only considering 100% observality
 
-    dataset_path = os.path.join(DATASET_ROOT, envname)
     envs = find_similar(envname)
     if ignore_noisy:
         envs = list(filter(lambda x: 'noisy' not in x, envs))
-    tar_files = []
     for o in obs:
         for env in envs:
             path = os.path.join(env, o)
@@ -42,8 +33,6 @@ def extract(envname, ignore_noisy=True, obs=['100']):
                     shutil.rmtree(tar.split('.')[0])
                 problem_name = re.findall('(block-words(-\w+)?_p\d+)', tar)[0][0]
                 print(problem_name)
-                # problem_name = '_'.join(tar.split('/')[-1].split('_')[:2])
-                # print(tar.split('/')[-1])
                 problems.add(problem_name)
 
             for problem_name in problems:
@@ -61,12 +50,8 @@ def extract(envname, ignore_noisy=True, obs=['100']):
                     ex_all = True
                 else:
                     ex_all = False
-                # set()
-                # print(fname)
                 hyp = re.findall(r'hyp-\d+', fname)[0].split('-')[-1]
-                with tarfile.open(fname) as f:
-                    # print(fname)
-                    
+                with tarfile.open(fname) as f:                    
                     _extract(f, hyp, ex_all)
             for problem_name in problems:
                 problem_path = os.path.join(path, problem_name)
@@ -78,18 +63,14 @@ def extract(envname, ignore_noisy=True, obs=['100']):
 
 def _extract(tar: tarfile.TarFile, hyp_idx: str, extract_all: bool):
     path = tar.name.split('/')
-    # name = path[-1].split('.')[0]
     problem_name = re.findall('(block-words(-\w+)?_p\d+)', tar.name)[0][0]
     dir_path = os.path.join('/'.join(path[:-1]), problem_name)
-    # print(dir_path)
     if extract_all:
         if os.path.exists(dir_path):
             shutil.rmtree(dir_path)
         os.mkdir(dir_path)
-        # print(tar.list())
         tar.extractall(dir_path)
     else:
-        # print(tar.list())
         tar.extract('./obs.dat', dir_path)
     os.rename(dir_path+'/obs.dat', dir_path+f'/obs_hyp-{hyp_idx}.dat')
     adapt_obs(dir_path+f'/obs_hyp-{hyp_idx}.dat')
@@ -97,5 +78,5 @@ def _extract(tar: tarfile.TarFile, hyp_idx: str, extract_all: bool):
 def find_similar(envname):
     return glob.glob(f'{DATASET_ROOT}/{envname}*')
 
-# print(find_similar('blocks'))
+
 print(extract('blocks'))
