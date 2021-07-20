46d047cc2a71d0c2f64b62552f4acb61d9053cb9
gabwaen <gabrielwaengertner@gmail.com>
2021-07-19 22:16:32 -0300

    Changed PDDL problem parser to pddl-parser

--- src/dataset/adapt_blocksworld.py
+++ src/dataset/adapt_blocksworld.py
@@ -1,20 +1,20 @@
+import sys
+import os
+
 BLOCKSWORLD_MAPPING = {
     "pick-up": "pickup",
     "handempty": "handempty robot",
 }
 
-ACTIONS_MAPPING = {
-
-}
-
 ACTIONS = ["pickup", "putdown", "unstack", "stack"]
 OBJECTS = []
 
-def gen_actions(blocks):
+def gen_actions(objects):
     putdown = []
     unstack = []
     pickup = []
     stack = []
+    blocks = objects['block']
     for i in range(len(blocks)):
         putdown.append(f'(putdown {blocks[i]})')
         unstack.append(f'(unstack {blocks[i]})')
@@ -25,42 +25,58 @@ def gen_actions(blocks):
             stack.append(f'(stack {blocks[i]} {blocks[j]})')
     return '\n'.join(putdown), '\n'.join(unstack), '\n'.join(pickup), '\n'.join(stack)
 
-def create_problem_files(template, goals, path):
+
+def create_problem_files(parser, template_name, goals, path):
     domain_name = path.split('/')[-1]
     files = []
-    with open(template, 'r') as t, open(goals, 'r') as g:
-        template_text = list(filter(lambda x: x != '', map(lambda x: x.rstrip('\n'), t.readlines())))
+    parser.parse_problem(template_name)
+    with open(goals, 'r') as g:
         goals_text = list(map(lambda x: x.rstrip('\n'), g.readlines()))
         for i, goal in enumerate(goals_text):
-            files.append(write_problem_file(goal, i, domain_name, template_text, path))
+            files.append(write_problem_file(parser, goal, i, domain_name, path))
     return files
 
-def extract_objects(objects):
-    return objects.split(' - block')[0].split(' ')
+def build_objects(parser):
+    objects_str = ''
+    for t in parser.objects:
+        objs = ' '.join(parser.objects[t])
+        objects_str += f'{objs} - {t}\n'
+    objects_str += 'robot - robot\n'
+    return objects_str
 
-def write_problem_file(goal, i, domain, template, path):
-    objects = []
+def build_initial_state(parser):
+    putdown, unstack, pickup, stack =  gen_actions(parser.objects)
+    literals = ''
+    for t in parser.state:
+        predicate = t[0]
+        # objs = t[1:]
+        if predicate == 'handempty':
+            literals += '(handempty robot)\n'
+        else:
+            literal = ' '.join(t)
+            literals += f'({literal})\n'
+    return f"{putdown}\n\
+        {unstack}\n\
+        {pickup}\n\
+        {stack}\n\
+        {literals}"
+
+    
+def write_problem_file(parser, goal, i, domain, path):
     file_path = f'{path}/{domain}_problem_{i:02d}.pddl'
+    goal = ' '.join(goal.split(','))
+    problem_definition = f"(define (problem blocks_words)\n\
+        (:domain {parser.domain_name})\n\
+        (:objects {build_objects(parser)})\n\
+        (:init {build_initial_state(parser)})\n\
+        (:goal (and {goal})))"
     with open(file_path, 'w') as f:
-        for line in template:
-
-            if ' - block' in line:
-                objects = extract_objects(line)
-                if ')' in line:
-                    line = line.rstrip(')') + ' robot - robot)'
-                else:
-                    line += ' robot - robot'
-            elif '(:init' in line:
-                putdown, unstack, pickup, stack =  gen_actions(objects)
-                line = '\n'.join([line, '\n', putdown, unstack, pickup, stack])
-            elif 'HANDEMPTY' in line:
-                line = '(handempty robot)'
-            elif '<HYPOTHESIS>' in line:
-                line = ' '.join(goal.split(','))
-            f.write(f'{line}\n')
+        
+        f.write(problem_definition)
+    
     return file_path
 
-def adapt_obs(obs_path):
+def adapt_observation(obs_path):
     with open(obs_path, 'r') as f:
         actions = f.readlines()
     with open(obs_path, 'w') as f:
@@ -69,7 +85,6 @@ def adapt_obs(obs_path):
             a = action_split[0].lower().lstrip('(')
             objs = action_split[1:]
             if a == 'pick-up':
-                print(a, objs)
                 a = 'pickup'
                 objs = objs[0]
             elif a == 'put-down':
--- src/dataset/extract_dataset.py
+++ src/dataset/extract_dataset.py
@@ -4,8 +4,15 @@ import os
 import glob
 import shutil
 import re
+import sys
 
-from adapt_blocksworld import create_problem_files, adapt_obs
+from adapt_blocksworld import create_problem_files, adapt_observation
+
+# the dirtiest workarounds to make submodules work lol
+sys.path.append(os.path.abspath(os.path.join('../..')))
+sys.path.append(os.path.abspath(os.path.join('../../pddl_parser')))
+
+from pddl_parser.PDDL import PDDL_Parser
 
 try:
     REPO_ROOT = os.environ['RLROOT']
@@ -15,11 +22,22 @@ except KeyError as e:
 DATASET_ROOT = f'{REPO_ROOT}/goal-plan-recognition-dataset'
 
 
+def create_parser(domain_path):
+    parser = PDDL_Parser()
+    parser.parse_domain(domain_path)
+    return parser
+
+def clear_parser(parser):
+    parser.state = None
+    parser.objects = {}
+
 # will only consider blocksworld for now
 def extract(envname, ignore_noisy=True, obs=['100']):
     # assumes that the dataset is located at root/goal-plan-recognition-dataset
     # and only considering 100% observality
-
+    pddlgym_path = '/'.join(pddlgym.__file__.split('/')[:-1])
+    domain_path = os.path.join(pddlgym_path, 'pddl', 'blocks.pddl')
+    parser = create_parser(domain_path)
     envs = find_similar(envname)
     if ignore_noisy:
         envs = list(filter(lambda x: 'noisy' not in x, envs))
@@ -32,12 +50,10 @@ def extract(envname, ignore_noisy=True, obs=['100']):
                 if os.path.exists(tar.split('.')[0]):
                     shutil.rmtree(tar.split('.')[0])
                 problem_name = re.findall('(block-words(-\w+)?_p\d+)', tar)[0][0]
-                print(problem_name)
                 problems.add(problem_name)
 
             for problem_name in problems:
                 problem_path = os.path.join(path, problem_name)
-                print(problem_path)
                 if os.path.exists(problem_path):
                     shutil.rmtree(problem_path)
                 os.mkdir(problem_path)
@@ -55,11 +71,12 @@ def extract(envname, ignore_noisy=True, obs=['100']):
                     _extract(f, hyp, ex_all)
             for problem_name in problems:
                 problem_path = os.path.join(path, problem_name)
-                pddl_paths = create_problem_files(problem_path+f'/template.pddl', problem_path+'/hyps.dat', problem_path)
+                pddl_paths = create_problem_files(parser, problem_path+'/template.pddl', problem_path+'/hyps.dat', problem_path)
                 for pddl in pddl_paths:
                     pddl_name = pddl.split('/')[-1]
                     save_path = '/'.join(pddlgym.__file__.split('/')[:-1])+f'/pddl/blocks/{pddl_name}'
                     shutil.copyfile(pddl, save_path)
+                clear_parser(parser)
 
 def _extract(tar: tarfile.TarFile, hyp_idx: str, extract_all: bool):
     path = tar.name.split('/')
@@ -73,7 +90,7 @@ def _extract(tar: tarfile.TarFile, hyp_idx: str, extract_all: bool):
     else:
         tar.extract('./obs.dat', dir_path)
     os.rename(dir_path+'/obs.dat', dir_path+f'/obs_hyp-{hyp_idx}.dat')
-    adapt_obs(dir_path+f'/obs_hyp-{hyp_idx}.dat')
+    adapt_observation(dir_path+f'/obs_hyp-{hyp_idx}.dat')
 
 def find_similar(envname):
     return glob.glob(f'{DATASET_ROOT}/{envname}*')
