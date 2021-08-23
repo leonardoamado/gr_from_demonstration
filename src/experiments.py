e42bc116124d027ab51aabd6942eab37416df737
Leonardo Amado <leonardo.amado@acad.pucrs.br>
2021-08-23 15:57:33 -0300

    Start of experiments

--- src/experiments.py
+++ src/experiments.py
@@ -1,8 +1,12 @@
 from recognizer import Recognizer
 
 #Define datasets here
-BLOCKS = ['output/blocks_gr']
+#Each element of this list is a goal recognition problem with 5 different observabilities
+BLOCKS = ['output/blocks_gr/']
 def run_experiments():
-    recog = Recognizer
+    recog = Recognizer()
+    for folder in BLOCKS:
+        recog.complete_recognition_folder(folder)
+
 if __name__ == "__main__":
     run_experiments()
